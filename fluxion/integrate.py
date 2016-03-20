9053b14d77235965d6d6c3635c87870775e5b816
Bogdan Opanchuk <bogdan@opanchuk.net>
2016-03-21 00:09:08 +1100

    integrate: add a debug plotting routine

--- fluxion/integrate.py
+++ fluxion/integrate.py
@@ -198,4 +198,28 @@ def integrate(eq, initial_field, pdim_start, seed=None, samplers={}):
 
 
 def plot(results):
-    raise NotImplementedError
+
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+    for key, f in results.items():
+        fig = plt.figure()
+        s = fig.add_subplot(1, 1, 1)
+
+        if len(f.dimensions) == 1:
+            s.plot(f.dimensions[0].grid, f.data)
+        elif len(f.dimensions) == 2:
+            dims = f.dimensions
+
+            if dims[0].uniform and dims[1].uniform:
+                s.imshow(
+                    f.data.T,
+                    extent=(dims[1].grid[0], dims[1].grid[-1], dims[0].grid[0], dims[0].grid[-1]),
+                    aspect='auto',
+                    cmap=matplotlib.cm.viridis)
+            else:
+                # use imshow(griddata(...))?
+                raise NotImplementedError
+
+        fig.savefig(key + '.pdf')
