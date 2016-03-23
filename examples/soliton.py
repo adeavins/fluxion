37a08d7da3297785c7a8337efc83cc9583a581ba
Bogdan Opanchuk <bogdan@opanchuk.net>
2016-03-23 21:54:49 +1100

    examples/soliton: remove an unused line

--- examples/soliton.py
+++ examples/soliton.py
@@ -13,7 +13,6 @@ eq = Eq(diff(psi, t), 0.5j * diff(psi, x, x) + (1j * abs(psi) - 0.5j) * psi)
 def xdensity(psi, t):
     return abs(psi)**2
 
-k = momentum_space(x)
 def kdensity(psi, t):
     psi_k, k = to_momentum_space(psi, x)
     return abs(psi_k)**2
