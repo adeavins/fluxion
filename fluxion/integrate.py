d8fb4021780fb757e34b29e51c8441d4c8f8721b
Bogdan Opanchuk <bogdan@opanchuk.net>
2016-03-27 12:24:02 +1100

    integrate: simplify sampler calling logic
    No need to call them in two places

--- fluxion/integrate.py
+++ fluxion/integrate.py
@@ -46,17 +46,6 @@ def sample_field(field, pdim):
     return field
 
 
-def sample(results, stepper, ufield_snapshot, samplers, events):
-    # TODO: must be rewritten as a pure function
-    for pdim_val, keys in events:
-        interp_field = as_field(stepper.interpolate_at(pdim_val), template=ufield_snapshot)
-        for key in keys:
-            results[key]['pvalue'].append(pdim_val)
-            result = samplers[key](interp_field, pdim_val)
-            result = as_field(result)
-            results[key]['mean'].append(result)
-
-
 def integrate(eq, initial_field, pdim_start, seed=None, samplers={}):
 
     stepper_gen = RK4Stepper(step=0.02)
@@ -66,39 +55,40 @@ def integrate(eq, initial_field, pdim_start, seed=None, samplers={}):
 
     tdimensions = [d for d in ufield.dimensions if d != pdimension]
     ufield_snapshot = ufield.without_dimensions(pdimension)
-    field = as_field(initial_field, template=ufield_snapshot)
+    field = as_field(initial_field, ufield_snapshot)
 
     sequences = {key: val[1] for key, val in samplers.items()}
     samplers = {key: val[0] for key, val in samplers.items()}
 
     seq = Sequence(sequences)
 
-    initial_samples = seq.pop_events_until(pdim_start)
-
     results = {key: dict(pvalue=[], mean=[]) for key in samplers}
 
-    # Stepper only deals with arrays, not fields
-    deriv_func = CallableExpr(eq.args[1], ufield, pdimension)
-    stepper = stepper_gen.initialize(pdim_start, field.data, deriv_func)
-
-    sample(results, stepper, ufield_snapshot, samplers, initial_samples)
+    stepper = stepper_gen(eq, pdim_start, field.data)
 
     while True:
+
+        pdim_val = stepper.pdim
+        to_sample = seq.pop_events_until(pdim_val)
+
+        if len(to_sample) > 0:
+            print(pdim_val)
+
+        # sample
+        for pdim_val, keys in events:
+            interp_field_arr = stepper.interpolate_at(pdim_val)
+            interp_field = as_field(interp_field_arr, ufield_snapshot)
+            for key in keys:
+                results[key]['pvalue'].append(pdim_val)
+                result = samplers[key](interp_field, pdim_val)
+                result = as_field(result)
+                results[key]['mean'].append(result)
 
         if seq.empty():
             break
 
         # propagate a step forward
         stepper.step()
-
-        pdim_val = stepper.pdim
-
-        to_sample = seq.pop_events_until(pdim_val)
-
-        if len(to_sample) > 0:
-            print(pdim_val)
-
-        sample(results, stepper, ufield_snapshot, samplers, to_sample)
 
     for key in results:
         generic_field = find_generic_field(results[key]['mean'], ufield.dimensions)
